6a7bb0c6b15289ebccbe29252f41acba2ec0399e
tgaillard1 <timlgaillard@gmail.com>
2025-08-22 16:02:09 +0000

    updated init with ask agent

--- pa_multiagent/sub_agents/__init__.py
+++ pa_multiagent/sub_agents/__init__.py
@@ -15,6 +15,7 @@
 from .bqml.agent import root_agent as bqml_agent
 from .analytics.agent import root_agent as ds_agent
 from .bigquery.agent import database_agent as db_agent
+from .ask.rag.agent import root_agent as ask_rag_agent
 
 
 __all__ = ["bqml_agent", "ds_agent", "db_agent", "ask_rag_agent"]
