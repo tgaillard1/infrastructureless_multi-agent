1604bfa3c456b0c260bcd3f02020586343d0a849
tgaillard1 <timlgaillard@gmail.com>
2025-08-26 14:25:40 +0000

    git pull

--- pa_multiagent/agent.py
+++ pa_multiagent/agent.py
@@ -76,6 +76,7 @@ root_agent = Agent(
     tools=[
         call_db_agent,
         call_ds_agent,
+        ask_rag_agent, # Add the new tool to the root agent
         load_artifacts,
     ],
     before_agent_callback=setup_before_agent_call,
