57f62376cbdcd2d0fa50c6611e89f2eaaf47976b
tgaillard1 <timlgaillard@gmail.com>
2025-09-02 10:04:53 -0700

    Update prompts.py
    changed tool to agent

--- pa_multiagent/prompts.py
+++ pa_multiagent/prompts.py
@@ -31,7 +31,7 @@ def return_instructions_root() -> str:
     - If the question needs SQL executions, forward it to the database agent.
     - If the question needs SQL execution and additional analysis, forward it to the database agent and the datascience agent.
     - If the user specifically wants to work on BQML, route to the bqml_agent.
-    - If the user's question relates to Principal Architect reference materials, guidelines, or best practices, you must use the **`ask_rag_agent`** tool.
+    - If the user's question relates to Principal Architect reference materials, guidelines, or best practices, route to the ask_rag_agent.
 
     - **IMPORTANT:** be precise! If the user asks for a dataset, provide the name. Don't call any additional agent if not absolutely necessary!
 
@@ -64,7 +64,7 @@ def return_instructions_root() -> str:
         #   A. You provide the fitting query.
         #   B. You pass the project and dataset ID.
         #   C. You pass any additional context.
-        #   * **RAG Agent `ask_rag_agent`:** Use this tool when the query is about principal architect best practices or reference materials, or other related technical topics like **"AI Adoption"**, **"Vertex AI"**, or other cloud products.
+        #   * **RAG Agent `ask_rag_agent`:** Use this agent when the query is about principal architect best practices or reference materials, or other related technical topics like **"AI Adoption"**, **"Vertex AI"**, or other cloud products.
 
         **Key Reminder:**
         * ** You do have access to the database schema! Do not ask the db agent about the schema, use your own information first!! **
