a9247a458ae8bc202060a8fec19d0ce7b1a527de
tgaillard1 <timlgaillard@gmail.com>
2025-12-22 16:14:42 +0000

    updated agent files with new reference to infra

--- infra_multiagent/sub_agents/bigquery/tools.py
+++ infra_multiagent/sub_agents/bigquery/tools.py
@@ -21,7 +21,7 @@ import re
 
 import numpy as np
 import pandas as pd
-from pa_multiagent.utils.utils import get_env_var
+from infra_multiagent.utils.utils import get_env_var
 from google.adk.tools import ToolContext
 from google.cloud import bigquery
 from google.genai import Client
--- infra_multiagent/sub_agents/bqml/agent.py
+++ infra_multiagent/sub_agents/bqml/agent.py
@@ -20,7 +20,7 @@ from google.adk.tools.agent_tool import AgentTool
 from google.adk.agents.callback_context import CallbackContext
 
 
-from pa_multiagent.sub_agents.bqml.tools import (
+from infra_multiagent.sub_agents.bqml.tools import (
     check_bq_models,
     execute_bqml_code,
     rag_response,
@@ -28,8 +28,8 @@ from pa_multiagent.sub_agents.bqml.tools import (
 from .prompts import return_instructions_bqml
 
 
-from pa_multiagent.sub_agents.bigquery.agent import database_agent as bq_db_agent
-from pa_multiagent.sub_agents.bigquery.tools import (
+from infra_multiagent.sub_agents.bigquery.agent import database_agent as bq_db_agent
+from infra_multiagent.sub_agents.bigquery.tools import (
     get_database_settings as get_bq_database_settings,
 )
 
